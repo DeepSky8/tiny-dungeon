1fb9f9e04314705712c4ec94c836fb81f3e0c039
DeepSky8 <deepsky8@gmail.com>
2023-05-09 12:58:24 -0400

    Add Header

---
+++ src/components/home/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header__container">
+            <h1>Tiny Dungeon</h1>
+
+        </div>
+    )
+}
+
+export default Header
