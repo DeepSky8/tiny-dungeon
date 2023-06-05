c99025d9477b3d1ed7a6fae14f2d87660a6b6ca7
DeepSky8 <DeepSky8@gmail.com>
2023-06-05 17:01:14 -0400

    Add create and display scroll tools

--- src/actions/scrollActions.js
+++ src/actions/scrollActions.js
@@ -1,19 +1,58 @@
+import { child, push, ref, update } from "firebase/database"
+import { db } from "../api/firebase"
+
 export const loadScroll = (scroll) => ({
     type: 'LOAD_SCROLL',
     scroll
 })
 
+export const clearScroll = () => ({
+    type: 'CLEAR_SCROLL'
+})
+
 export const updateSID = (sID) => ({
-    type: 'UPDATE_S',
+    type: 'UPDATE_SID',
     sID
 })
 
-export const updateSName = (sName) => ({
-    type: 'UPDATE_S',
-    sName
+export const updateSTitle = (sTitle) => ({
+    type: 'UPDATE_STITLE',
+    sTitle
 })
 
-export const updateSText = (sText) => ({
-    type: 'UPDATE_S',
-    sText
+export const updateSDescription = (sDescription) => ({
+    type: 'UPDATE_SDESCRIPTION',
+    sDescription
 })
+
+export const updateSAmount = (sAmount) => ({
+    type: 'UPDATE_SAMOUNT',
+    sAmount
+})
+
+
+export const startSaveScroll = async ({
+    sID,
+    sTitle,
+    sDescription,
+    sAmount
+
+}) => {
+    const updates = {}
+
+    updates[`scrolls/${sID}/sID`] = sID
+    updates[`scrolls/${sID}/sTitle`] = sTitle
+    updates[`scrolls/${sID}/sDescription`] = sDescription
+    updates[`scrolls/${sID}/sAmount`] = sAmount
+
+    update(ref(db), updates)
+        .catch((error) => {
+            console.log('Did not save scroll', error)
+        })
+}
+
+
+export const startNewScrollKey = async ({ scrollData }) => {
+    const sID = push(child(ref(db), 'scrolls')).key
+    startSaveScroll({ ...scrollData, sID })
+}
